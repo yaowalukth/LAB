ce001a80f69f34ffb044ad9f5be934574491bda9
yaowalukth <16917yaowaluk@gmail.com>
2024-09-20 14:46:35 +0700

    Create 10.cpp

---
+++ 10.cpp
@@ -0,0 +1,65 @@
+/*
+    จงเขียนโปรแกรมเพื่อรับคำจากผู้ใช้งาน เพื่อตรวจสอบว่า คำที่กรอกมามีลักษณะเป็นคำหรือวลีที่สามารถอ่านจากหลังไปหน้าหรือหน้าไปหลังแล้วยังคงความหมายเหมือนเดิมได้
+    โดยที่ หากคำนั้นสามารถอ่านจากหน้าไปหลังหรือหลังไปได้ ให้แสดงผลลัพธ์ว่า Pass แต่หากทำไม่ได้ให้ขึ้นว่า Not Pass
+
+    Test case:
+        Enter word:
+            radar
+    Output:
+        Pass.
+
+    Test case:
+        Enter word:
+            hello
+    Output:
+        Not Pass.
+
+    Test case:
+        Enter word:
+            Radar
+    Output:
+        Pass.
+
+    Test case:
+        Enter word:
+            here
+    Output:
+        Not Pass.
+    - strcpy copy
+    - strcmp compare
+    - strlen concatenation
+    - strcat count word
+*/
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char Word[ 100 ] ;
+    printf( "Enter your word : " ) ;
+    scanf( "%s", Word ) ;
+    printf( "Your word is : %s \n", Word ) ;
+  
+    int e = strlen( Word ) - 1 ;
+    printf( "Size of Word is : %d \n", e + 1 ) ;
+
+    int s = 0 ;
+    int n = 1 ;
+    while ( s < e ) {
+        if( Word[ s ] != Word[ e ] ) {
+            n = 0 ;
+            break;
+        } else {
+            s ++ ;
+            e -- ;
+        }//end if-else
+    }//end while
+
+    if( n == 0 ) {
+        printf( "Not Pass \n" ) ;
+    } else {
+        printf( "Pass \n" ) ;
+    }//end if-else
+
+    return 0 ;
+}//end main function
