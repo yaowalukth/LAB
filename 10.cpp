dd3efda6749c9fd1334bb2c56470e5e1dfde21d9
yaowalukth <16917yaowaluk@gmail.com>
2024-09-21 17:29:52 +0700

    Update 10.cpp

--- 10.cpp
+++ 10.cpp
@@ -32,6 +32,7 @@
 */
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main() {
 
@@ -39,14 +40,14 @@ int main() {
     printf( "Enter your word : " ) ;
     scanf( "%s", Word ) ;
     printf( "Your word is : %s \n", Word ) ;
-  
+
     int e = strlen( Word ) - 1 ;
     printf( "Size of Word is : %d \n", e + 1 ) ;
 
-    int s = 0 ;
-    int n = 1 ;
+    int s = 0 ; //ค่าเริ่มต้นของตำอักษรแรก
+    int n = 1 ; //ค่าบูลีน
     while ( s < e ) {
-        if( Word[ s ] != Word[ e ] ) {
+        if( tolower( Word[ s ] ) != tolower( Word[ e ] ) ) {
             n = 0 ;
             break;
         } else {
